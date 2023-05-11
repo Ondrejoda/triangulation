721bb8ab74c0bdfe980b25d957bc57d65adc6461
Ondrejoda <54633773+Ondrejoda@users.noreply.github.com>
2023-05-11 10:28:24 +0200

    sflfwld;

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+radio.setGroup(144)
+radio.setFrequencyBand(7)
+
+basic.forever(function() {
+    radio.sendNumber(1)
+    basic.showNumber(1)
+    basic.pause(250)
+    basic.clearScreen()
+    basic.pause(750)
+})
 
